9cf04fbf3061476da30672afdb73b88bfed8611f
Azdi <azdi.kashapov@yandex.ru>
2025-01-27 22:23:37 +0300

    V3

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-const buttons = document.querySelectorAll('.list .btn')
-
-buttons.forEach(item => {
-  item.addEventListener('click', () => {
-    buttons.forEach(btn => btn.classList.remove('active__btn'))
-    item.classList.add('active__btn')
-  })
-})
-
